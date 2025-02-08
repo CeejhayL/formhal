bccf0912419f76b14725b5947a50fa03cde0aad0
CeejhayL <lerioscyrusjohn@gmail.com>
2025-02-08 14:10:13 +0800

    Updated

--- script.js
+++ script.js
@@ -48,3 +48,34 @@ function resetPasscode() {
     currentIndex = 0;
     inputs.forEach(input => input.value = "");
 }
+const images = [
+    { src: "images/img1.jpg", desc: "First" },
+    { src: "image2.jpg", desc: "Description 2" },
+    { src: "image3.jpg", desc: "Description 3" },
+    { src: "image4.jpg", desc: "Description 4" },
+    { src: "image5.jpg", desc: "Description 5" },
+    { src: "image6.jpg", desc: "Description 6" },
+    { src: "image7.jpg", desc: "Description 7" },
+    { src: "image8.jpg", desc: "Description 8" },
+    { src: "image9.jpg", desc: "Description 9" },
+    { src: "image10.jpg", desc: "Description 10" }
+];
+
+let galleryIndex = 0;
+
+    function updateGallery() {
+        document.getElementById("galleryImage").src = images[galleryIndex].src;
+        document.getElementById("galleryDescription").textContent = images[galleryIndex].desc;
+    }
+
+    function nextImage() {
+        galleryIndex = (galleryIndex + 1) % images.length;
+        updateGallery();
+    }
+
+    function prevImage() {
+        galleryIndex = (galleryIndex - 1 + images.length) % images.length;
+        updateGallery();
+    }
+
+    updateGallery();
